9bab24564bd36d4ea0bd13a856a273ec771e3941
aiosima69 <aiosima69@gmail.com>
2026-01-10 21:37:13 +0700

    add sql backup

---
+++ database/sql/nutrient.sql
@@ -0,0 +1,13 @@
+INSERT INTO "nutrient" ("id", "name", "category") VALUES
+                                                      (1, 'Energy', 'Macronutrient'),
+                                                      (2, 'Protein', 'Macronutrient'),
+                                                      (3, 'Total lipid (fat)', 'Macronutrient'),
+                                                      (4, 'Carbohydrate, by difference', 'Macronutrient'),
+                                                      (5, 'Fiber, total dietary', 'Macronutrient'),
+                                                      (6, 'Cholesterol', 'Macronutrient'),
+                                                      (7, 'Vitamin A, RAE', 'Vitamin'),
+                                                      (8, 'Vitamin B12', 'Vitamin'),
+                                                      (9, 'Calcium, Ca', 'Mineral'),
+                                                      (10, 'Iron, Fe', 'Mineral');
+
+
